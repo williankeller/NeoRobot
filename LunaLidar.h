c2e751966d5d02528828b4413261b47c44dc4dc7
Willian Keller <williank@ciandt.com>
2023-04-12 00:43:14 -0400

    Improve LunaLidar code

--- LunaLidar.h
+++ LunaLidar.h
@@ -1,44 +1,54 @@
-
-#define HEADER     0x59
-#define FRAME_SIZE 9
+const uint8_t HEADER = 0x59;
+const uint8_t FRAME_SIZE = 9;
 
 void setupLidar() {
+  // Initialize serial and wait for port to open: 
   Serial1.begin(115200);
+
+  delay(50);
 }
 
-bool lidarData(int16_t &dist, int16_t &strength, int16_t &temp) {
+bool lidarData(int16_t &dist, int16_t &flux, int16_t &temp) {
 
-  int check;
-  int uart[9];
-  int i;
+  uint8_t frame[FRAME_SIZE];
+  uint8_t checksum = 0;
 
-  if (Serial1.available()) {
-    if (Serial1.read() == HEADER) {
-      uart[0] = HEADER;
-      if (Serial1.read() == HEADER) {
+  // Flush all but last frame of data from the serial buffer.
+  while (Serial1.available() > FRAME_SIZE) Serial1.read();
 
-        uart[1] = HEADER;
-        for (i = 2; i < 9; i++) {
-          uart[i] = Serial1.read();
-        }
-        check = uart[0] + uart[1] + uart[2] + uart[3] + uart[4] + uart[5] + uart[6] + uart[7];
-
-        if (uart[8] == (check & 0xff)) {
-          dist = uart[2] + (uart[3] << 8);
-          strength = uart[4] + (uart[5] << 8);
-          temp = uart[6] + (uart[7] << 8);
-
-          // Convert temp code to degrees Celsius.
-          temp = (temp >> 3) - 256;
-
-          
-        }
-      }
+  // Search for HEADER bytes in the incoming data
+  uint32_t serialTimeout = millis() + 1000;
+  while ((frame[0] != HEADER) || (frame[1] != HEADER)) {
+    if (Serial1.available()) {
+      memmove(frame, frame + 1, FRAME_SIZE - 1);
+      frame[FRAME_SIZE - 1] = Serial1.read();
     }
-    return true;
-  } else {
-    Serial.println("TF Lidar not available yet.");
-    delay(1000);
+
+    if (millis() > serialTimeout) {
+      Serial.println("TF Lidar not available yet.");
+      delay(1000);
+      return false;
+    }
+  }
+
+  // Calculate checksum
+  for (uint8_t i = 0; i < FRAME_SIZE - 1; i++) {
+    checksum += frame[i];
+  }
+
+  // Verify checksum
+  if (frame[FRAME_SIZE - 1] != (checksum & 0xFF)) {
+    Serial.println("Checksum error.");
     return false;
   }
+
+  // Extract and process data
+  dist = frame[2] + (frame[3] << 8);
+  flux = frame[4] + (frame[5] << 8);
+  temp = frame[6] + (frame[7] << 8);
+
+  // Convert temp code to degrees Celsius
+  temp = (temp >> 3) - 256;
+
+  return true;
 }
--- NeoRobotMain.ino
+++ NeoRobotMain.ino
@@ -1,16 +1,37 @@
-
+#include "LunaLidar.h"
 #include "Kinematics.h"
 
-void setup() {
-  // Initialize serial and wai for port to open:
-  Serial.begin(9600);
-  // This delay gives the chance to wait for a Serial Monitor without blocking if none is found.
-  delay(1500);
 
-  setupKinematics();
+void setup() {
+  // Initialize serial and wait for port to open:
+  Serial.begin(9600);
+
+  //setupKinematics();
+  setupLidar();
 }
 
+// Initialize variables
+int16_t tfDist = 0;  // Distance to object in centimeters
+int16_t tfFlux = 0;  // Strength or quality of return signal
+int16_t tfTemp = 0;  // Internal temperature of Lidar sensor chip
+
 void loop() {
+
+  if (lidarData(tfDist, tfFlux, tfTemp)) {
+
+    Serial.print("Distance=");
+    Serial.print(tfDist);
+    Serial.print('\t');
+    Serial.print("Strength=");
+    Serial.print(tfFlux);
+    Serial.print('\t');
+    Serial.print("Temp=");
+    Serial.print(tfTemp);
+    Serial.print('\t');
+    Serial.print('\n');
+    delay(50);
+  }
+
 
   if (Serial.available()) {
     int angle = Serial.parseInt();
@@ -21,10 +42,10 @@ void loop() {
       setLegSectionAngle(frontRightLeg.middle, angle);
     }
   }
-  
+
 
   //changeState(WALKING);
-  
+
   //walkForward(5, GAIT_STEP_DURATION);
   //changeState(STANDING);
 }
